09636cb9e50fbf4e4b025b1fd1ea655251cf5c81
YohannTisserand <tisserand.yohann@gmail.com>
2022-02-28 20:59:36 +0000

    database updated

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Shuttr = require('../models/shutter')
+const { places, descriptors } = require('./seedHelpers')
 const cities = require('./cities');
 
 async function main() {
@@ -9,12 +10,15 @@ async function main() {
 
 main().catch(err => console.log(err));
 
+const sample = array => array[Math.floor(Math.random() * array.length)]
+
 const seedDb = async () => {
   await Shuttr.deleteMany({})
   for (let i = 0; i < 10; i++) {
     const random1000 = Math.floor(Math.random() * 10);
     const shutter = new Shuttr({
-      location: `${cities[random1000].city}`
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`
     })
     await shutter.save();
   }
