d268aa98289de678d39118e5efbc7ad1528bd3dd
YohannTisserand <tisserand.yohann@gmail.com>
2022-02-28 13:34:35 +0000

    first test passing

---
+++ models/shutter.test.js
@@ -0,0 +1,28 @@
+const shutter = require('./shutter');
+const {MongoClient} = require('mongodb');
+
+describe('insert', () => {
+  let connection;
+  let db;
+  beforeAll(async () => {
+    connection = await MongoClient.connect(global.__MONGO_URI__, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = await connection.db(global.__MONGO_DB_NAME__);
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  it('should insert a doc into collection', async () => {
+    const global = db.collection('ShutterSchema');
+
+    const mockSchema = {_id: 'some-id', location: 'west-hampstead'};
+    await global.insertOne(mockSchema);
+
+    const insertedSchema = await global.findOne({_id: 'some-id'});
+    expect(insertedSchema).toEqual(mockSchema);
+  });
+});
