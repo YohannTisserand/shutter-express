81b55741414586f05454392c792105b01476adc0
YohannTisserand <tisserand.yohann@gmail.com>
2022-03-01 14:02:16 +0000

    added some styling

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./app.js')
 const PORT = process.env.PORT || 3000
 const mongoose = require('mongoose');
+const test = require('./models/shutter')
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
 
