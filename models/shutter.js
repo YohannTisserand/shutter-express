cef06be05c397cb412e3d0f371c20b3be6aa3baa
YohannTisserand <tisserand.yohann@gmail.com>
2022-03-01 11:43:25 +0000

    img added

--- models/shutter.js
+++ models/shutter.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const ShutterSchema = new Schema({
   title: String,
+  image: String,
   location: String,
   description: String
 })
--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Shutter = require('../models/shutter')
 const { places, descriptors } = require('./seedHelpers')
 const cities = require('./cities');
+const axios = require('axios')
 
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/shutter');
@@ -18,11 +19,14 @@ const seedDb = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const shutter = new Shutter({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://placeimg.com/640/480/nature',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium voluptates doloribus, eligendi numquam placeat ea! Quam adipisci, tempora mollitia molestiae dicta molestias nisi ea officiis illum fugit autem facilis numquam.'
     })
     await shutter.save();
   }
 }
+
 seedDb().then(() => {
   mongoose.connection.close();
 });
