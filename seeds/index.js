de73a500f167adb33acd5f6706f1390476e5e3c2
YohannTisserand <tisserand.yohann@gmail.com>
2022-03-01 12:24:29 +0000

    added some styling

--- seeds/index.js
+++ seeds/index.js
@@ -13,14 +13,28 @@ main().catch(err => console.log(err));
 
 const sample = array => array[Math.floor(Math.random() * array.length)]
 
+async function seedImg() {
+  try {
+    const resp = await axios.get('https://api.unsplash.com/photos/random', {
+      params: {
+        client_id: 'rMCZ09ezcx3cFDD7kX9E6OR8MSTpbqEDosYe02bu6mc',
+        collections: 1114848,
+      },
+    })
+    return resp.data.urls.small
+  } catch (err) {
+    console.error(err)
+  }
+}
+
 const seedDb = async () => {
   await Shutter.deleteMany({})
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 3; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const shutter = new Shutter({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://placeimg.com/640/480/nature',
+      image: await seedImg(),
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium voluptates doloribus, eligendi numquam placeat ea! Quam adipisci, tempora mollitia molestiae dicta molestias nisi ea officiis illum fugit autem facilis numquam.'
     })
     await shutter.save();
